bbaee5ce69b915bf312baf5d5a2d8509d475d698
Sky Deng <sky.deng.online@outlook.com>
2025-03-11 16:00:32 -0400

    add footer icons

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, MatIconModule],
-  exports: [CommonModule, FormsModule, MatIconModule],
+  imports: [CommonModule, FormsModule],
+  exports: [CommonModule, FormsModule],
 })
 export class SharedModule {}
