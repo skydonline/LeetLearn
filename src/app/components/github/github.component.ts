2597537fecfa2a2e65a002d218c5fbee00dd6b0d
Sky Deng <sky.deng.online@outlook.com>
2025-03-12 22:10:50 -0400

    create heading component

--- src/app/components/github/github.component.ts
+++ src/app/components/github/github.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ToggleComponent } from '../toggle/toggle.component';
+import { HeadingComponent } from '../heading/heading.component';
 
 @Component({
   selector: 'app-github',
-  imports: [ToggleComponent],
+  imports: [ToggleComponent, HeadingComponent],
   templateUrl: './github.component.html',
   styleUrl: './github.component.css',
 })
--- src/app/components/heading/heading.component.ts
+++ src/app/components/heading/heading.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-heading',
   imports: [],
   templateUrl: './heading.component.html',
-  styleUrl: './heading.component.css'
+  styleUrl: './heading.component.css',
 })
 export class HeadingComponent {
-
+  @Input() text: string = 'Heading';
 }
