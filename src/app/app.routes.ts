9a5a8265ba4a3cba725fa59279401ff65e4726c6
Sky Deng <sky.deng.online@outlook.com>
2025-03-12 21:35:25 -0400

    create basic routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { GithubComponent } from './components/github/github.component';
+import { CustomizationComponent } from './components/customization/customization.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/github',
+    pathMatch: 'full',
+  },
+  { path: 'github', component: GithubComponent },
+  { path: 'customization', component: CustomizationComponent },
+];
