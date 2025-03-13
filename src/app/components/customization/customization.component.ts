e962547170b2aeed07610b698acb79f2e91a4743
Sky Deng <sky.deng.online@outlook.com>
2025-03-12 21:09:59 -0400

    create github/customization components

---
+++ src/app/components/customization/customization.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customization',
+  imports: [],
+  templateUrl: './customization.component.html',
+  styleUrl: './customization.component.css'
+})
+export class CustomizationComponent {
+
+}
